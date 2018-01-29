ca17fda607757bc296a080805bc17a32364c7a97
hcomere <harold.comere@gmail.com>
2018-01-29 10:20:51 +0100

    * CodingGame community Temperatures puzzle

---
+++ csharp/CodePuzzles/CodingGame_classic_Temperatures/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+#if LOCAL
+using Validation;
+#endif
+
+class Solution
+{
+    static void Solve()
+    {
+        int n = int.Parse(Console.ReadLine()); // the number of temperatures to analyse
+        string[] inputs = Console.ReadLine().Split(' ');
+
+        Console.WriteLine(Enumerable
+            .Range(0, n)
+            .Select(i => int.Parse(inputs[i]))
+            .Select(t => new Tuple<int, int>(t, Math.Abs(t)))
+            .OrderBy(tp => tp.Item2)
+            .ThenByDescending(tp => tp.Item1) 
+            .Select(tp => tp.Item1)
+            .FirstOrDefault()
+        );
+    }
+
+    static void Main(string[] args)
+    {
+#if LOCAL
+        SolveAction handler = Solve;
+        Validation.Validator.ValidateSolution(handler);
+#else
+        Solve();
+#endif
+    }
+}
