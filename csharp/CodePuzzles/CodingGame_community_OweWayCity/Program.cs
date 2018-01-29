9028fc4315a7176a82c82b02bf44777700141314
hcomere <harold.comere@gmail.com>
2018-01-29 10:19:34 +0100

    * Coding game community One Way City puzzle

--- csharp/CodePuzzles/CodingGame_community_OweWayCity/Program.cs
+++ csharp/CodePuzzles/CodingGame_community_OweWayCity/Program.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Collections.Generic;
 using System.Numerics;
+using System.Diagnostics;
 
 #if LOCAL
 using Validation;
@@ -9,55 +10,54 @@ using Validation;
 
 class Solution
 {
+    static BigInteger Factoriel(BigInteger a_nb)
+    {
+        if (a_nb == 0)
+            return 0;
+
+        BigInteger res = 1;
+        BigInteger curr = a_nb;
+
+        while (curr > 1)
+        {
+            res *= curr;
+            --curr;
+        }
+
+        return res;
+    }
+
     static void Solve()
     {
+        Stopwatch stopWatch = new Stopwatch();
+        stopWatch.Start();
+
         int M = int.Parse(Console.ReadLine());
         int N = int.Parse(Console.ReadLine());
 
-        Console.Error.WriteLine(M + " WE, " + N + " NS");
-
-        if (M > N)
-        {
-            Console.Error.WriteLine("SWAP");
-            int tmp = M;
-            M = N;
-            N = tmp;
-        }
-
-        string res = "";
+        string res;
 
         if (M == 1 || N == 1)
             res = "1";
         else
         {
-            List<BigInteger> prevRow = Enumerable.Range(0, N).Select(v => new BigInteger(0)).ToList();
-
-            for (int row = 0; row < M; ++row)
-            {
-                for (int col = row; col < N; ++col)
-                {
-                    //Console.Error.WriteLine(row + " " + col);
-
-                    if (row == col)
-                    {
-                        if (row != 0)
-                            prevRow[col] *= 2;
-                    }
-                    else if (row == 0)
-                        prevRow[col] = 1;
-                    else
-                        prevRow[col] += prevRow[col - 1];
-                }
-            }
-
-            res = prevRow[N - 1].ToString("");
+            BigInteger pathLen = (M - 1) + (N - 1);
+            BigInteger result = Factoriel(pathLen) / (Factoriel(M - 1) * Factoriel(pathLen - (M - 1)));
+            res = result.ToString();
         }
 
-        Console.WriteLine(res);
+        if (res.Length <= 1000)
+            Console.WriteLine(res);
+        else
+            Console.WriteLine(res.Substring(0, 1000));
+
+        Console.Error.WriteLine("Done in " + stopWatch.ElapsedMilliseconds + " ms");
     }
 
     static void Main(string[] args)
     {
+
+
 #if LOCAL
         SolveAction handler = Solve;
         Validation.Validator.ValidateSolution(handler);
